b9b3d1a07c098739569f96c8dbe8d1b3cd6562a8
Herman Jensen <herman.jensen@intility.no>
2017-03-27 15:48:30 +0200

    site?

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import facebook from './img/facebook-logo.png';
+import soundcloud from './img/soundcloud-logo.png';
+import github from './img/github-sign.png';
+import steam from './img/steam.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="Social">
+          <a href="https://facebook.com/hermanj" target="_blank" className="Social-link">
+            <img src={facebook} className="Social-icon" alt="facebook" />
+          </a>
+          <a href="https://soundcloud.com/herman-jensen" target="_blank" className="Social-link">
+            <img src={soundcloud} className="Social-icon" alt="soundcloud" />
+          </a>
+          <a href="https://github.com/h3rmanj" target="_blank" className="Social-link">
+            <img src={github} className="Social-icon" alt="github" />
+          </a>
+          <a href="http://steamcommunity.com/id/the0hj/" target="_blank" className="Social-link">
+            <img src={steam} className="Social-icon" alt="steam" />
+          </a>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
